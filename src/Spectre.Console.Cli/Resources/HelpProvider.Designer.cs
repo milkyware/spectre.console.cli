cc3290e32ec5c691d47fcb683fe064a04e602c91
Cameron Cowen <cameronc94@gmail.com>
2025-11-25 16:24:40 +0000

    added exit code header translation resources

--- src/Spectre.Console.Cli/Resources/HelpProvider.Designer.cs
+++ src/Spectre.Console.Cli/Resources/HelpProvider.Designer.cs
@@ -19,7 +19,7 @@ namespace Spectre.Console.Cli.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class HelpProvider {
@@ -111,6 +111,15 @@ namespace Spectre.Console.Cli.Resources {
         internal static string Examples {
             get {
                 return ResourceManager.GetString("Examples", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to EXIT CODES.
+        /// </summary>
+        internal static string ExitCodes {
+            get {
+                return ResourceManager.GetString("ExitCodes", resourceCulture);
             }
         }
         
